cafd19a2ab1ded3a1c36a55eeb6a0f3d9be2a83b
shyna sharma <shyna.passi8@gmail.com>
2021-11-04 00:19:36 +0530

    googlekeepApp

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const Footer=()=> {
+  return (
+    <>
+    <div className="footer">
+        
+        <p>Copyright</p>
+    </div>
+    </>
+  );
+}
+
+export default Footer;
